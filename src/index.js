f2656d7ff5306e1bf58c18876eec579302e48ce9
Keegan Marsh Sanctis <keegansanctis@gmail.com>
2025-05-12 11:07:21 +0300

    Optimized page scroll to top on opening new page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
